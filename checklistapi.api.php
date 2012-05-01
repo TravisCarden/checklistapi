a1555d718b82b5fb2d3d8449ed8d3b5410dc2dba
TravisCarden <TravisCarden@236758.no-reply.drupal.org>
2012-04-30 22:05:04 -0500

    Switched to #attribute style array keys on checklist definitions.

--- checklistapi.api.php
+++ checklistapi.api.php
@@ -31,11 +31,11 @@
  */
 function hook_checklistapi_checklist_info_alter(&$checklists) {
   // Add an item.
-  $checklists['example']['groups']['example_group']['items']['sample_item'] = array(
+  $checklists['example']['example_group']['sample_item'] = array(
     'title' => t('Sample item'),
   );
   // Remove an item.
-  unset($checklists['example']['groups']['example_group']['items']['example_item']);
+  unset($checklists['example']['example_group']['example_item']);
 }
 
 /**
@@ -48,39 +48,40 @@ function hook_checklistapi_checklist_info_alter(&$checklists) {
  *   An array of checklists. Each checklist is keyed by an arbitrary unique
  *   identifier. The corresponding multidimensional array describing the
  *   checklist may contain the following key-value pairs:
- *   - title: Required. The title of the checklist.
- *   - description: A brief description of the checklist for its corresponding
+ *   - #title: Required. The title of the checklist.
+ *   - #description: A brief description of the checklist for its corresponding
  *     menu item.
- *   - path: Required. The Drupal path where the checklist will be accessed.
- *   - menu_name: Set this to a custom menu if you don't want your item to be
+ *   - #path: Required. The Drupal path where the checklist will be accessed.
+ *   - #menu_name: Set this to a custom menu if you don't want your item to be
  *     placed in Navigation.
- *   - weight: An integer used to sort the list of checklists before being
+ *   - #weight: An integer used to sort the list of checklists before being
  *     output; lower numbers appear before higher numbers.
- *   - groups: An array of containers for grouping items, presented as vertical
+ *   - Any number of arrays representing groups of items, presented as vertical
  *     tabs. Each group is keyed by an arbitrary unique identifier. The
  *     corresponding multimensional array describing the group may contain the
  *     following key-value pairs:
- *     - title: Required. The title of the group used as the vertical tab label.
- *     - description: A description of the group.
- *     - weight: An integer used to sort the list of groups before being output;
- *       lower numbers appear before higher numbers.
- *     - items: An array of checklist items. Each item is keyed by an arbitrary
- *       unique identifier. The corresponding multimensional array describing
- *       the item may contain the following key-value pairs:
- *       - title: Required. The title of the item.
- *       - description: A description of the item.
- *       - default_value: The default checked state of the item--TRUE for
+ *     - #title: Required. The title of the group used as the vertical tab
+ *       label.
+ *     - #description: A description of the group.
+ *     - #weight: An integer used to sort the list of groups before being
+ *       output; lower numbers appear before higher numbers.
+ *     - Any number of arrays representing checklist items. Each item is keyed
+ *       by an arbitrary unique identifier. The corresponding multimensional
+ *       array describing the item may contain the following key-value pairs:
+ *       - #title: Required. The title of the item.
+ *       - #description: A description of the item.
+ *       - #default_value: The default checked state of the item--TRUE for
  *         checked or FALSE for unchecked. Defaults to FALSE. This is useful for
  *         automatically checking off tasks that can be programmatically tested
  *         (e.g. a module is installed or a setting set).
- *       - weight: An integer used to sort the list of items before being
+ *       - #weight: An integer used to sort the list of items before being
  *         output; lower numbers appear before higher numbers.
- *       - links: An array of links. Each link is keyed by an arbitrary unique
- *         identifier. The corresponding multimensional array describing the
- *         link may contain the following key-value pairs:
- *         - text: The link text.
- *         - path: The link path.
- *         - context: The context in which the link may appear. May be one of
+ *       - Any number of arrays representing links. Each link is keyed by an
+ *         arbitrary unique identifier. The corresponding multimensional array
+ *         describing the link may contain the following key-value pairs:
+ *         - #text: The link text.
+ *         - #path: The link path.
+ *         - #context: The context in which the link may appear. May be one of
  *           the following:
  *           - CHECKLISTAPI_LINK_CONTEXT_ANY: Default. The link will always
  *             appear.
@@ -88,8 +89,8 @@ function hook_checklistapi_checklist_info_alter(&$checklists) {
  *             the item it belongs to has been checked off.
  *           - CHECKLISTAPI_LINK_CONTEXT_ITEM_UNCHECKED: The link will appear if
  *             the item it belongs to has not checked off.
- *         - options: An associative array of additional options used by the l()
- *           function.
+ *         - #options: An associative array of additional options used by the
+ *           l() function.
  *
  * For a detailed usage example, see checklistapi_example.module.
  *
@@ -100,24 +101,18 @@ function hook_checklistapi_checklist_info() {
   $checklists = array();
 
   $checklists['example'] = array(
-    'title' => t('Example checklist'),
-    'description' => t('An example checklist.'),
-    'path' => 'example-checklist',
-    'help' => t('<p>This is an example checklist.</p>'),
-    'groups' => array(
-      'example_group' => array(
-        'title' => t('Example group'),
-        'description' => t('<p>Here are some example items.</p>'),
-        'items' => array(
-          'example_item' => array(
-            'title' => t('Example item'),
-            'links' => array(
-              'example_link' => array(
-                'text' => t('Example.com'),
-                'path' => 'http://www.example.com/',
-              ),
-            ),
-          ),
+    '#title' => t('Example checklist'),
+    '#description' => t('An example checklist.'),
+    '#path' => 'example-checklist',
+    '#help' => t('<p>This is an example checklist.</p>'),
+    'example_group' => array(
+      '#title' => t('Example group'),
+      '#description' => t('<p>Here are some example items.</p>'),
+      'example_item' => array(
+        '#title' => t('Example item'),
+        'example_link' => array(
+          'text' => t('Example.com'),
+          'path' => 'http://www.example.com/',
         ),
       ),
     ),
