1ef7a9c82f6cdfd8eba11577e7aba715571c7e17
TravisCarden <TravisCarden@236758.no-reply.drupal.org>
2012-07-10 22:52:57 -0500

    By TravisCarden: Made "Show/Hide descriptions" link dynamic (no page load).

--- checklistapi.js
+++ checklistapi.js
@@ -2,9 +2,10 @@
 
 Drupal.behaviors.checklistapiFieldsetSummaries = {
   attach: function (context) {
+
+    // Vertical tabs summaries.
     $('#checklistapi-checklist-form .vertical-tabs-panes > fieldset', context).drupalSetSummary(function (context) {
-      var total = $(':checkbox', context).size();
-      var args = {};
+      var total = $(':checkbox', context).size(), args = {};
       if (total) {
         args['@complete'] = $(':checkbox:checked', context).size();
         args['@total'] = total;
@@ -12,6 +13,18 @@ Drupal.behaviors.checklistapiFieldsetSummaries = {
         return Drupal.t('@complete of @total (@percent%) complete', args);
       }
     });
+
+    // Compact mode link.
+    $('#checklistapi-checklist-form .compact-link a', context).click(function () {
+      var is_compact_mode = $(this).closest('#checklistapi-checklist-form').hasClass('compact-mode');
+      $(this)
+        .text((is_compact_mode) ? Drupal.t('Hide descriptions') : Drupal.t('Show descriptions'))
+        .attr('title', (is_compact_mode) ? Drupal.t('Compress layout by hiding descriptions.') : Drupal.t('Expand layout to include descriptions.'))
+        .closest('#checklistapi-checklist-form').toggleClass('compact-mode');
+      document.cookie = 'Drupal.visitor.checklistapi_compact_mode=' + ((is_compact_mode) ? 0 : 1);
+      return false;
+    });
+
   }
 };
 
