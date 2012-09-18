2219b45364b85ab06336c988220739802b3a752c
TravisCarden <travis.carden@gmail.com>
2012-09-18 11:25:10 -0700

    By TravisCarden: Made it clear that "Hide descriptions" link only affects item descriptions.

--- checklistapi.js
+++ checklistapi.js
@@ -27,8 +27,8 @@
         $(this).closest('#checklistapi-checklist-form').toggleClass('compact-mode');
         var is_compact_mode = $(this).closest('#checklistapi-checklist-form').hasClass('compact-mode');
         $(this)
-          .text((is_compact_mode) ? Drupal.t('Show descriptions') : Drupal.t('Hide descriptions'))
-          .attr('title', (is_compact_mode) ? Drupal.t('Expand layout to include descriptions.') : Drupal.t('Compress layout by hiding descriptions.'))
+          .text((is_compact_mode) ? Drupal.t('Show item descriptions') : Drupal.t('Hide item descriptions'))
+          .attr('title', (is_compact_mode) ? Drupal.t('Expand layout to include item descriptions.') : Drupal.t('Compress layout by hiding item descriptions.'))
         document.cookie = 'Drupal.visitor.checklistapi_compact_mode=' + ((is_compact_mode) ? 1 : 0);
         return false;
       });
