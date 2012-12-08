e4f38f8db05918436a9aaac83a79f70962add6a2
TravisCarden <travis.carden@gmail.com>
2012-12-08 17:58:31 -0600

    By TravisCarden: JSLinted.

--- checklistapi.js
+++ checklistapi.js
@@ -27,9 +27,9 @@
         $(this).closest('#checklistapi-checklist-form').toggleClass('compact-mode');
         var is_compact_mode = $(this).closest('#checklistapi-checklist-form').hasClass('compact-mode');
         $(this)
-          .text((is_compact_mode) ? Drupal.t('Show item descriptions') : Drupal.t('Hide item descriptions'))
-          .attr('title', (is_compact_mode) ? Drupal.t('Expand layout to include item descriptions.') : Drupal.t('Compress layout by hiding item descriptions.'))
-        document.cookie = 'Drupal.visitor.checklistapi_compact_mode=' + ((is_compact_mode) ? 1 : 0);
+          .text(is_compact_mode ? Drupal.t('Show item descriptions') : Drupal.t('Hide item descriptions'))
+          .attr('title', is_compact_mode ? Drupal.t('Expand layout to include item descriptions.') : Drupal.t('Compress layout by hiding item descriptions.'));
+        document.cookie = 'Drupal.visitor.checklistapi_compact_mode=' + (is_compact_mode ? 1 : 0);
         return false;
       });
     }
